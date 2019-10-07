f8cb5ee139d725187e81570484820dc4673721d3
zhangjun <674241013@qq.com>
2019-10-07 11:23:54 +0800

    小程序分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,18 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast() {
+      wx.showToast({
+        title: '加载中ing',
+        icon:'loading'
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 设置分享转发
+  onShareAppMessage:function(options){
+  return {
+    title:'你好!',
+    // 别人点击时进入的哪个页面的地址
+    path:'/pages/about/about',
+    imageUrl:'http://pic39.nipic.com/20140321/18063302_210604412116_2.jpg'
+  }
   }
 })
